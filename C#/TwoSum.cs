3261498864143bd0ac0111b8848412ea43b07f5c
szczepaf <szczepanik@centrum.cz>
2022-07-10 00:13:29 +0200

    First problem added in both languages.

---
+++ C#/TwoSum.cs
@@ -0,0 +1,22 @@
+﻿namespace Namespace
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int > seen = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (seen.ContainsKey(target - nums[i])) return new int[] { seen[target - nums[i]], i };
+                else seen[nums[i]] = i;
+
+               
+            }
+            return null;
+
+        }
+        
+    }
+
+}
